ea3d0818c9ff594e74e6f5a61d88c3915d351771
Jonas Kirkemyr <jonas@fosen-utvikling.no>
2015-10-14 15:41:18 +0200

    removed license

--- Cloud_App/model/tileMsg.js
+++ Cloud_App/model/tileMsg.js
@@ -1,5 +1,11 @@
 /**
- * Created by Jonas on 12.10.2015.
+ * Tiles project
+ * Project at NTNU
+ *
+ * Project: Cloud_App
+ * Author: Jonas
+ * 12.10.2015
+ * First created in v0.1.0
  */
-
 //TODO: CREATE MSG FORMAT HERE TO SEND TO CLIENTS
+"use strict";
