39f3c84d221d8c0a3d9b2f1377d6806e944a4d0c
Jonas Kirkemyr <jonas@fosen-utvikling.no>
2015-10-29 15:43:59 +0100

    added encryption of user model password

--- Tiles CLOUD/APIserver/app/models/User.js
+++ Tiles CLOUD/APIserver/app/models/User.js
@@ -5,17 +5,25 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR = 10;
+var SALT_WORK_FACTOR = 10;//strength of encryption (10=default bcrypt value)
 
 //user - crypt password
 // @src: http://blog.mongodb.org/post/32866457221/password-authentication-with-mongoose-part-1
+/**
+ * User Schema
+ */
 var UserSchema = new Schema({
     username: {type: String, required: true, index: {unique: true}},
     email: {type: String},
     password: {type: String, required: true}
 });
 
-UserSchema.pre(save, function (next) {
+/**
+ * Function run before a user is saved to db
+ * to make sure the new password is stored encrypted
+ * using brcypt
+ */
+UserSchema.pre('save', function (next) {
     var user = this;
 
     //hash password if modified
