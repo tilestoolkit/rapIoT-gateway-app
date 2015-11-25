10f71747438dd0cce23f5399ab1a3acfa69082c6
Jonas Kirkemyr <jonas@fosen-utvikling.no>
2015-11-25 13:16:37 +0100

    added python tileclient library with example usage

---
+++ Tiles CLIENTS/python/tileclient/tileclient/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['TileApi','TileClient','TileListener']
+
+# deprecated to keep older scripts who import this from breaking
+from tileclient.TileApi import TileApi
+from tileclient.TileClient import TileClient
+from tileclient.TileListener import TileListener
