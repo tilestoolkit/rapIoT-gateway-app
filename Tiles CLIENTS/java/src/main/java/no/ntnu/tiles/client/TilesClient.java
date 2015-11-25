ccbdb9b7f27a746f0ec86396c6cf3d0fc28a90d7
sivapalan <varuns@stud.ntnu.no>
2015-11-25 14:54:08 +0100

    Added Java client

---
+++ Tiles CLIENTS/java/src/main/java/no/ntnu/tiles/client/TilesClient.java
@@ -0,0 +1,107 @@
+package no.ntnu.tiles.client;
+import java.util.Random;
+
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
+/**
+ * A client for event-driven (asynchronous) communication with the Tiles server
+ */
+public class TilesClient {
+	
+	private static final String HOST = "tcp://test.mosquitto.org";
+	
+	private MqttAsyncClient mqttClient;
+	private MqttCallbackHandler mqttCallbackHandler;
+	
+	private String username;
+	private TilesCallback tilesCallback;
+	
+	/**
+	 * @param tilesCallback A callback listener to use for asynchronous events. 
+	 * @param username Your username
+	 * @see TilesCallback
+	 */
+	public TilesClient(TilesCallback tilesCallback, String username){
+		this.tilesCallback = tilesCallback;
+		this.username = username;
+		this.mqttCallbackHandler = new MqttCallbackHandler();
+	}
+	
+	/**
+	 * Connects to the server.
+	 * {@link TilesCallback#connected()} is called when the client is successfully connected to the server.
+	 */
+	public void connect(){
+		try {
+			mqttClient = new MqttAsyncClient(TilesClient.HOST, "JavaClient_"+new Random().nextInt(99999));
+	    	mqttClient.setCallback(mqttCallbackHandler);
+	    	mqttClient.connect(null, mqttCallbackHandler);
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Sends a message to all clients listening for messages for the Tile with the provided ID
+	 * @param tileId The ID of the Tile this message is intended for
+	 * @param message A {@link String} representation of the message
+	 */
+	public void send(String tileId, String message){
+		send(tileId, message.getBytes());
+	}
+	
+	/**
+	 * Sends a message to all clients listening for messages for the Tile with the provided ID
+	 * @param tileId The ID of the Tile this message is intended for
+	 * @param bytes The bytes to be sent
+	 */
+	public void send(String tileId, byte[] bytes){
+		try {
+			mqttClient.publish("tiles/"+this.username+"/"+tileId, new MqttMessage(bytes));
+		} catch (MqttPersistenceException e) {
+			e.printStackTrace();
+		} catch (MqttException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private class MqttCallbackHandler implements MqttCallback, IMqttActionListener {
+		public void messageArrived(String topic, MqttMessage message) throws Exception {
+			String[] splitTopic = topic.split("/");
+			String deviceId = splitTopic[2];
+			if (splitTopic.length > 3 && splitTopic[3].equals("active")){
+				if (new String(message.getPayload()).equals("true")){
+					tilesCallback.tileRegistered(deviceId);
+				} else {
+					tilesCallback.tileUnregistered(deviceId);
+				}
+			} else {
+				tilesCallback.messageArrived(deviceId, message.getPayload());
+			}
+		}
+		
+		public void onSuccess(IMqttToken asyncActionToken) {
+	    	try {
+				mqttClient.subscribe(new String[]{"tiles/" + username + "/+", "tiles/" + username + "/+/active"}, new int[] {0, 0});
+				tilesCallback.connected();
+			} catch (MqttException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public void connectionLost(Throwable cause) { }
+
+		public void deliveryComplete(IMqttDeliveryToken token) { }
+
+		public void onFailure(IMqttToken asyncActionToken, Throwable exception) { }
+	}
+
+}
