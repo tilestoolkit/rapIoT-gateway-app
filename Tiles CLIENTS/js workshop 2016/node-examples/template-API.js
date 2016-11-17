8e4d26b8571924421cbbdcc10cb8237ab7c443a2
andersri <anderrma@stud.ntnu.no>
2016-11-17 14:31:46 +0100

    fixing error with templates

--- Tiles CLIENTS/js workshop 2016/node-examples/template-API.js
+++ Tiles CLIENTS/js workshop 2016/node-examples/template-API.js
@@ -3,8 +3,8 @@ var WorkshopClient = require('../workshop-client');
 var client = new WorkshopClient.TilesClient('Anders', '138.68.144.206', 1883).connect();
 var reader = new WorkshopClient.EventReader();
 var HueClient = new WorkshopClient.HueClient('PCz7eZwSifbpmLTQCdMVVvFEfC3MB7-odXvFAzC4');
-var SpotifyClient = new workshopClient.PostmanClient('192.168.1.6', 3000);
-var IFTTTClient = new workshopClient.IFTTTClient('dncBS7n2aVGR4Bf44Yx9Ck');
+var SpotifyClient = new WorkshopClient.PostmanClient('192.168.1.6', 3000);
+var IFTTTClient = new WorkshopClient.IFTTTClient('dncBS7n2aVGR4Bf44Yx9Ck');
 
 client.on('receive', function (tileId, event) {
 	console.log(event);
