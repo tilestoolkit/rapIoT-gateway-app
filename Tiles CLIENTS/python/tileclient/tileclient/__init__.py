764692cf593e537cfaf729ee3980f77af4f9cb82
Simone Mora <morasimone@gmail.com>
2016-11-29 17:03:21 +0100

    refactoring clients

--- Tiles CLIENTS/python/tileclient/tileclient/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ['TileApi','TileClient','TileListener']
-
-# deprecated to keep older scripts who import this from breaking
-from tileclient.TileApi import TileApi
-from tileclient.TileClient import TileClient
-from tileclient.TileListener import TileListener
