4516b5fceb0f78a0e057fc254d5df0a001d74e68
Andrea Nornes <andreanornes@Andreas-MacBook-Air.local>
2017-02-05 22:05:41 +0100

    Add html to the home page from the old code. Most of it is commented out as it is not turned into angular 2 yet. Also adds devices.sservices as a provider for the home component.

--- GATEWAY_2/tilesgateway/src/pages/home/home.ts
+++ GATEWAY_2/tilesgateway/src/pages/home/home.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { Device, DevicesService } from '../../providers/devices.service';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	devices: Device[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+  						private devicesService: DevicesService) {
+  	this.devices = devicesService.getMockDevices();
+  };
 
-  }
-
-}
+};
