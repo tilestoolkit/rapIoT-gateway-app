bb2741acb8245a3c77183fe9f30fe4fd7e27f507
Andrea Nornes <andreanornes@gmail.com>
2017-02-13 21:15:57 +0100

    Documentation for the tilesApi

--- GATEWAY_2/tilesgateway/src/providers/tilesApi.service.ts
+++ GATEWAY_2/tilesgateway/src/providers/tilesApi.service.ts
@@ -3,6 +3,11 @@ import { Http }    from '@angular/http';
 import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/toPromise';
 
+import { Device } from './devices.service';
+
+/** 
+ * Class to describe the structure of a command 
+ */
 class CommandObject {
   name: string;
   properties: string;
@@ -38,7 +43,10 @@ export class TilesApi {
 							private http: Http) {
 	};
 
-	// Returns an object with name and properties from the inputstring
+  /** 
+   * Returns an object with name and properties from the inputstring
+   * @param {string} eventString - A string on the format eventName,properties...
+   */
   getEventStringAsObject = (eventString: string) => {
     const params = eventString.split(',');
     return {
@@ -47,15 +55,24 @@ export class TilesApi {
     };
   };
 
-  // Turn the eventobject into a string
+  /** 
+   * Returns a string from the given commandObject
+   * @param {CommansObject} cmdObj - the command to turn into a string
+   */
   getCommandObjectAsString = (cmdObj: CommandObject) => {
     return cmdObj.name + ',' + cmdObj.properties.toString();
   };
 
-  // Create a new object that has all the attributes from both inputobjects
-  extend = (obj1, obj2) => {
+  /** 
+   * Create a new object that has all the attributes from both inputobjects
+   * @param {any} obj1 - The first object
+   * @param {any} obj2 - The second object
+   */
+  extend = (obj1: any, obj2: any) => {
+    // TODO: Find out if the objects passed in are of a specific type
     let extended = {};
     // TODO: if any attr has the same name obj 2 will overwrite obj1
+    // could be fixed by checking if the attrname exists and then renaming it
     for (let attrname of obj1) {
       extended[attrname] = obj1[attrname];
     }
@@ -65,20 +82,41 @@ export class TilesApi {
     return extended;
   };
 
-  isTilesDevice = (device: any) => (device.name != null && device.name.substring(0, 4) === 'Tile');
+  /** 
+   * Tests if a device is a tile
+   * @param {Device} device - the device to test
+   */
+  isTilesDevice = (device: Device) => (device.name != null && device.name.substring(0, 4) === 'Tile');
 
+  /** 
+   * Set a username for the tile owner/user
+   * @param {string} username - The new username
+   */
   setUsername = (username: string) => {
     this.username = username;
   };
 
+  /** 
+   * Set the host address
+   * @param {string} hostAddress - The url/ip address of the host
+   */
   setHostAddress = (hostAddress: string) => {
     this.hostAddress = hostAddress;
   };
 
-  setHostMqttPort = (hostMqttPort) => {
+  /** 
+   * Set the port for connecting to the server
+   * @param {number} hostMqttPort - the port number 
+   */
+  setHostMqttPort = (hostMqttPort: number) => {
     this.mqttPort = hostMqttPort;
   };
 
+  /** 
+   * TODO: I cannot see this (or the loadEventMappings working as the eventmappings are objects, not arrays
+   * and does not even have any of the fields asked for. 
+   * All the functions below needs to be rewritten. 
+   */
   getEventMapping = (tileId, eventAsString) => {
     if (this.eventMappings[this.username] == null ||
         this.eventMappings[this.username][tileId] == null) {
