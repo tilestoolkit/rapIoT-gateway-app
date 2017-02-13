d03ebd95f8ca179ee16d04ecc000c5869bc7af2b
Andrea Nornes <andreanornes@gmail.com>
2017-02-13 15:49:56 +0100

    Documentation for the devices.service

--- GATEWAY_2/tilesgateway/src/providers/devices.service.ts
+++ GATEWAY_2/tilesgateway/src/providers/devices.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 
-// Class for the devices, this makes it possible to specify the
-// device type in typescript to avoid getting invalid device-objects
+/** 
+ * Class for the devices, this makes it possible to specify the
+ * device type in typescript to avoid getting invalid device-objects
+ */
 export class Device {
   id: string;
   name: string;
@@ -17,17 +19,27 @@ export class DevicesService {
     this.devices = [];
   };
 
+  /** 
+   * Returns mock devices for testing purposes
+   */
   getMockDevices = (): Device[] => ([
   	{id: '01:23:45:67:89:AB', name: 'TI SensorTag1', connected: false, ledOn: false},
   	{id: '01:23:45:67:89:AC', name: 'TI SensorTag2', connected: true, ledOn: true},
   	{id: '01:23:45:67:89:AD', name: 'TI SensorTag3', connected: false, ledOn: false},
   ]);
 
+  /** 
+   * Returns the list of devices currently stored
+   */
   getDevices = (): Device[] => {
     alert('providing devices from devicesService: ' + this.devices.toString())
   	return this.devices;
   };
 
+  /** 
+   * Adds a new device to the list of devices
+   * @param {Device} deviceId - the device to add
+   */
   newDevice = (device: Device) => {
     if (!this.devices.includes(device)){
       this.devices.push(device);
@@ -35,6 +47,10 @@ export class DevicesService {
     }
   };
 
+  /** 
+   * Converts the device discovered by ble into a device on the tiles format
+   * @param {any} bleDevice - the returned device from the ble scan
+   */
   convertBleDeviceToDevice = (bleDevice: any): Device  => {
     /*const device: Device = {
       id: '01:23:45:67:89:AB', 
