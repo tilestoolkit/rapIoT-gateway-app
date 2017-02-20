1412dcb059b82f43e9dce5ef076c54f0c23a5c51
TheHeadmaster95 <theheadmaster95@gmail.com>
2017-02-20 16:19:28 +0100

    Fixed cross-platform cli problem

--- GATEWAY/test/providers/devices.service.spec.ts
+++ GATEWAY/test/providers/devices.service.spec.ts
@@ -5,8 +5,17 @@ import { DevicesService, Device } from '../../src/providers/devices.service';
 // if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
 
 describe('Test-test on devices.service', () => {
+    beforeEach( () => {
+        this.devicesService = new DevicesService();
+    });
+
   it('should have an empty list "devices" after construction', () => {
     const devices = new DevicesService();
     assert.equal(devices.getDevices().length, 0);
   });
+
+  it('running getMockDevices should return three devices', () => {
+      const devices = this.devicesService.getMockDevices();
+      assert.equal(devices.length, 3);
+  })
 });
