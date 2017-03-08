3be706ae3caa06307a8524bdeba2a53e8350181b
TheHeadmaster95 <theheadmaster95@gmail.com>
2017-03-08 10:46:28 +0100

    Save 'progress' on testing

--- GATEWAY/test/app/app.component.spec.ts
+++ GATEWAY/test/app/app.component.spec.ts
@@ -1,19 +1,40 @@
 import 'reflect-metadata';
 import 'mocha';
+import 'zone.js';
 import { assert, expect } from 'chai';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
+import { PlatformMock } from '../fixtures/mocks/mock';
 import { AppVersion, StatusBar, Splashscreen } from 'ionic-native';
+import { TestBed, ComponentFixture, async, inject } from '@angular/core/testing';
+//import { IonicModule } from 'ionic-angular';
 
-
+import { Tiles } from '../../src/app/app.component';
+let comp: Tiles;
+let fixture: ComponentFixture<Tiles>;
+let window;
+ 
 describe('test import of platform', () => {
-
+ 
     beforeEach(() => {
-    const window = { location : { host : 'example.com' } };
-        
-    });
+        this.window = { location : { host : 'example.com' } };
+        const platformMock = new PlatformMock();
+        /*
+        TestBed.configureTestingModule({
+ 
+            declarations: [
+                Tiles
+            ],
+             
+            providers: [
 
+            ]
+ 
+        }).compileComponents();
+        */
+    });
+        
     it('should work', () => {
-        assert.equal(true, true);
-    })
+            assert.equal(true, true);
+    });
 });
