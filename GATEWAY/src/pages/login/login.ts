8c7ffa3066952c9dcd556ae6d0de7c99aa53ec41
Eivind Sandell <Eivind Sandell>
2017-03-21 15:23:52 +0100

    Login page update, missing utils

--- GATEWAY/src/pages/login/login.ts
+++ GATEWAY/src/pages/login/login.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { MqttClient } from '../../providers/mqttClient';
+import { TilesApi } from '../../providers/tilesApi.service';
+import { UtilsService } from '../../providers/utils.service';
 
 
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html'
+  templateUrl: 'login.html',
+    providers: [
+        TilesApi,
+        MqttClient,
+        UtilsService,
+    ],
 })
 export class LoginPage {
   loginInfo = {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      private mqttClient: MqttClient,
+      private tilesApi: TilesApi,
+      private  utils: UtilsService,)
+  {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
@@ -18,5 +32,14 @@ export class LoginPage {
   loginForm(){
 
   }
+    /*
+    connectToServer = (user: string, host: string, port: number): void => {
+        if (this.verifyLoginCredentials(user, host, port)) {
+            this.mqttClient.connect(user, host, port);
+        } else {
+            alert("Invalid login credentials.");
+        }
+    }
+    */
 
 }
