9f6d4854230885717275ce167465349a1d9f5f5e
Kevin Midboe <KevinMidboe@dhcp-10-22-34-141.wlan.ntnu.no>
2017-03-21 14:01:34 +0100

    Added simple functionality for navigation between pages.

--- GATEWAY/src/pages/tabs/tabs.ts
+++ GATEWAY/src/pages/tabs/tabs.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+
 import { HomePage } from '../home/home';
+import { PhysicalTilesPage } from '../physical-tiles/physical-tiles';
+import { DevTermPage } from '../dev-term/dev-term';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -8,6 +11,8 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   rootPage: any = HomePage;
+  physicalPage: any = PhysicalTilesPage;
+  devPage: any = DevTermPage;
 
   constructor() {
 
