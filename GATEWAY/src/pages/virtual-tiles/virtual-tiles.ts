1711fd12fad465595a058db2b9071d9348e59b5e
Kevin Midboe <KevinMidboe@dhcp-10-22-34-52.wlan.ntnu.no>
2017-03-22 14:25:16 +0100

    Functions for getting the id variable from navcontroller and displaying it in title.

--- GATEWAY/src/pages/virtual-tiles/virtual-tiles.ts
+++ GATEWAY/src/pages/virtual-tiles/virtual-tiles.ts
@@ -7,16 +7,22 @@ import { NavController, NavParams } from 'ionic-angular';
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+
+// function for capitalizing a string 
+var capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
+
 @Component({
   selector: 'page-virtual-tiles',
   templateUrl: 'virtual-tiles.html'
 })
 export class VirtualTilesPage {
+	applicationTitle: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	// A id variable is stored in the navParams, and .get set this value to the local variable id
+  	let id = navParams.get('_id');
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VirtualTilesPage');
+  	// Sets the title of the page (found in virtual-tiles.html) to id, capitalized. 
+  	this.applicationTitle = capitalize(id);
   }
-
 }
