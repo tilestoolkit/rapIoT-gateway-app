60494275f5521c65e7edb57e5f09981133fec2e3
Kevin Midboe <KevinMidboe@dhcp-10-22-34-52.wlan.ntnu.no>
2017-03-22 14:23:08 +0100

    Added functionality for connecting (will be removed later and placed in login). Now also navigates to application view when pressed and sends id variable to new view through navcontroller.

--- GATEWAY/src/pages/applications/applications.ts
+++ GATEWAY/src/pages/applications/applications.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { ModalController } from 'ionic-angular';
 import { ModalPage } from './modal-page';
 import { LoginPage } from '../login/login';
-
 import { VirtualTilesPage } from '../virtual-tiles/virtual-tiles';
+
+import { TilesApi } from '../../providers/tilesApi.service';
+import { Application, UtilsService } from '../../providers/utils.service';
+import { MqttClient } from '../../providers/mqttClient';
 
 /*
   Generated class for the Applications page.
@@ -14,15 +17,24 @@ import { VirtualTilesPage } from '../virtual-tiles/virtual-tiles';
 */
 @Component({
   selector: 'page-applications',
-  templateUrl: 'applications.html'
+  templateUrl: 'applications.html',
+  providers: [TilesApi]
 })
 export class ApplicationsPage {
   loggedIn = true;
+  applications: Application[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {}
+  constructor(public navCtrl: NavController, 
+  						public navParams: NavParams, 
+  						public modalCtrl: ModalController,
+  						public alertCtrl: AlertController,
+  						private mqttClient: MqttClient,
+  						private tilesApi: TilesApi,
+  						private utils: UtilsService) {}
 
   ionViewDidLoad() {
-    console.log("applications loaded")
+    console.log("applications loaded");
+
     if(!this.loggedIn){
       this.presentModal();
     }
@@ -34,10 +46,34 @@ export class ApplicationsPage {
     modal.present();
   }
 
-  goToApplicationView() {
+
+  setApplications = (): void => {
+    this.tilesApi.getAllApplications().then( data => {
+       this.applications = data;
+      console.log('DATA: ' + data);
+    }).catch (err => console.log(err));
+  }
+
+  connectToServer = (user: string, host: string, port: number): void => {
+		if (this.utils.verifyLoginCredentials(user, host, port)) {
+			this.mqttClient.connect(user, host, port);
+		} else {
+			alert("Invalid login credentials.");
+		}
+	}
+
+	showConnectMQTTPopup = () => {
+		this.connectToServer('andrea', '172.68.99.218', parseInt('8080'));
+    this.setApplications();
+	};
+
+
+
+  viewApplication = (application: Application): void => {
     //push another page onto the history stack
     //causing the nav controller to animate the new page in
-    this.navCtrl.push(VirtualTilesPage);
+    this.navCtrl.push(VirtualTilesPage, {
+    	_id: application._id
+    });
   }
 }
-
