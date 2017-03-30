ba57b096c2aa5d5e26e572dd38e27ad14401f274
Andrea Nornes <andreanornes@gmail.com>
2017-03-30 08:07:26 +0200

    have working build with all scripts failing..

--- GATEWAY/karma.conf.js
+++ GATEWAY/karma.conf.js
@@ -36,6 +36,13 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    customLaunchers: {
+    // chrome setup for travis CI using chromium
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: [' — no-sandbox']
+      }
+    },
   });
 };
