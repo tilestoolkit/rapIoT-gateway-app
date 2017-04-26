07cdda0ae63fe866a073df2cb683efef09b453a1
Andrea Nornes <andreanornes@gmail.com>
2017-04-26 15:02:40 +0200

    add error alert for errors when calling on the api

--- GATEWAY/src/providers/tilesApi.service.ts
+++ GATEWAY/src/providers/tilesApi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, Response }    from '@angular/http';
 import { Storage } from '@ionic/storage';
+import { AlertController } from 'ionic-angular';
 import 'rxjs/add/operator/toPromise';
 
 import { Application, LoginData, VirtualTile } from './utils.service';
@@ -15,7 +16,8 @@ export class TilesApi {
   private virtualTiles: VirtualTile[] = [];
   private loginData: LoginData;
 
-  constructor(public http: Http,
+  constructor(private alertCtrl: AlertController, 
+	      public http: Http,
               public storage: Storage) {
   }
 
@@ -116,8 +118,13 @@ export class TilesApi {
               }
             })
             .catch(err => {
-              return false;
-              // alert('failed getting applications with error: ' + err);
+              try {
+                if (err.status === 0) {
+                  this.presentErrorAlert();
+                }
+              } finally {
+                return false;
+              }
             });
   }
 
@@ -132,7 +139,13 @@ export class TilesApi {
               return res.json();
             })
             .catch(err => {
-              console.log('failed getting applications with error: ' + err);
+              try {
+                if (err.status === 0) {
+                  this.presentErrorAlert();
+                }
+              } finally {
+                console.log('failed getting applications with error: ' + err);
+              }
             });
   }
 
@@ -148,9 +161,15 @@ export class TilesApi {
               return res.json();
             })
             .catch(err => {
-              console.log('failed getting applications with error: ' + err);
-              console.log('url ' + url);
-              return null;
+              try {
+                if (err.status === 0) {
+                  this.presentErrorAlert();
+                }
+              } finally {
+                console.log('failed getting applications with error: ' + err);
+                console.log('url ' + url);
+                return null;
+              }
             });
   }
 
@@ -175,7 +194,26 @@ export class TilesApi {
     console.log('url: ' + url + ' body: ' + body);
     return this.http.post(url, body, {headers: headerFields}).toPromise()
              .catch(err => {
-               console.log('Feiled pairing of the physical and virtual tile with error: ' + err);
+                try {
+                  if (err.status === 0) {
+                    this.presentErrorAlert();
+                  }
+                } finally {
+                  console.log('Feiled pairing of the physical and virtual tile with error: ' + err);
+                }
              });
+  }
+
+  /**
+  */
+  private presentErrorAlert = (): void => {
+    this.alertCtrl.create({
+      title: 'Could not get data from remote source',
+      subTitle: 'Make sure you have internet connection and that you have provided the correct host address.' +
+                 'If it still won\'t work the error might be on the host. Try again later or contact the host owners.',
+      buttons: [{
+        text: 'Dismiss',
+      }]
+    }).present();
   }
 }
