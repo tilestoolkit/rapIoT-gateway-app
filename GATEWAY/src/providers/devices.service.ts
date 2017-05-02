d9a8749870f988de052cab3aec7aeccccac619be
Andrea Nornes <andreanornes@gmail.com>
2017-05-02 14:47:25 +0200

    shorten time for device to disappear from list

--- GATEWAY/src/providers/devices.service.ts
+++ GATEWAY/src/providers/devices.service.ts
@@ -81,7 +81,7 @@ export class DevicesService {
   public clearDisconnectedDevices = (): void => { // TODO: Change name?
     const currentTime = (new Date()).getTime();
     this.devices = this.devices.filter(device => {
-      return currentTime - device.lastDiscovered < 15000 || device.connected;
+      return currentTime - device.lastDiscovered < 8000 || device.connected;
     });
     this.events.publish('updateDevices');
   }
